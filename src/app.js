c6b294ede4e0e76f644d1f1abc83934565806145
Jefferson Dias Fraga <jeffycordan@gmail.com>
2020-07-01 12:43:21 -0300

    Routes added

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const App = () => {
         <BrowserRouter>
             <div>
                 <nav>
-                    <ul>
+                    <ul /* aqui eu crio os links */> 
                         <li> <Link to="/sign-in"> Sing In </Link> </li>
                         <li> <Link to="/sign-up"> Sing Up </Link> </li>
                         <li> <Link to="/manage/links/create"> Create Link </Link> </li>
@@ -15,7 +15,7 @@ const App = () => {
                         <li> <Link to="/manage/links"> Links </Link> </li>
                     </ul>
                 </nav>
-                <Switch>
+                <Switch /* aqui é onde o link se redireciona */>
                     <Route path="/sign-in"><h1>Sing In</h1></Route>
                     <Route path="/sign-up"><h1>Sing Up</h1></Route>
                     <Route path="/manage/links/create"><h1>Create Link</h1></Route>
