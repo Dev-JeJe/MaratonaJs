fdc61707797362ecfac1ce21c6d6d47af28121a6
Jefferson Dias <jeffycordan@gmail.com>
2020-07-01 08:22:10 -0300

    Excluded auth path from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -2,6 +2,16 @@
 const { verifyJwt } = require('../helpers/jwt');
 
 const checkJwt = (req, res, next) => {
+    // as rotas de sign-in e sign-up não é para ter verificação de token
+    const { url: path } = req;
+
+    const excludedPaths = ['/auth/sign-in', '/auth/sign-up'];
+
+    const isExcluded = !!excludedPaths.find( p => p.startsWith(path)); //verifica se algum dos path de sign estiver na requisição 
+    //(!!) → muda a varivável para boolean
+    if (isExcluded) return next(); //se for um path que não precise de validação de token
+
+    console.log(path, isExcluded);
     //esse jwt será passado em todas as requisições, num header 'authorization'
     let token = req.headers['authorization'];
     token = token ? token.slice(7, token.length) : null ;
