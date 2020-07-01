b9163628481a3cb733e950c7e4d7d89b557c7365
Jefferson Dias Fraga <jeffycordan@gmail.com>
2020-07-01 19:09:12 -0300

    Screens created

---
+++ src/screens/Layouts/Manage/index.js
@@ -0,0 +1,25 @@
+import React, { Children } from 'react';
+
+const Layout = ({children}/* prop*/) => {
+    /* const {children = prop}*/
+    return (
+        <div className="layout">
+            <nav className="navbar navbar-expnd-lg bg-primary text-light">
+                <div className="container d-flex w-100 justify-content-between">
+                    <div>
+                        <span>BACK</span>
+                    </div>
+                    <div className="text-center">
+                        <strong>Links</strong>
+                    </div>
+                    <div>
+                        <span>Exit</span>
+                    </div>
+                </div>
+            </nav>
+            <div className="container">{children}</div>
+        </div>
+    );
+};
+
+export default Layout;
