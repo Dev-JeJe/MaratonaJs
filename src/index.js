50c9f6c83b23ddb2575ce33bcbfedf4c4c377268
Jefferson Dias Fraga <jeffycordan@gmail.com>
2020-07-01 14:30:05 -0300

    Install Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root')); //renderiza o que precisar na div root na pasta public
 
