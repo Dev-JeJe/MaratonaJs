350509773740a080f645ab0025c3d90c256c4d00
Jefferson Dias Fraga <jeffycordan@gmail.com>
2020-07-01 17:39:51 -0300

    Addeed UI Kit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
-import './main.scss';
+import './styles/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root')); //renderiza o que precisar na div root na pasta public
 
