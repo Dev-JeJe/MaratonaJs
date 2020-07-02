fbbdb7ee2239abfba8e75ba92c5d572ef1e6eda7
Jefferson Dias Fraga <jeffycordan@gmail.com>
2020-07-02 12:14:23 -0300

    Account init

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //aplicação para o ReactDOM
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'; //componentes do react router dom
 
 import Home from './screens/Home';
@@ -9,7 +10,14 @@ import ManageLinks from './screens/Manage/Links';
 import ManageLinksCreate from './screens/Manage/Links/Create';
 import ManageLinksEdit from './screens/Manage/Links/Edit';
 
-const App = () => {
+import { initAccount } from './actions/AccountActions';
+
+
+const App = ({ initAccount }) => {
+    useEffect(() => {
+        initAccount();
+    }, [initAccount]);
+
     return (
         <BrowserRouter>
             <div>
@@ -35,4 +43,8 @@ const App = () => {
     );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+    return {account: state.account.account};
+};
+
+export default connect(mapStateToProps, {initAccount})(App);
--- src/reducers/AccountReducer.js
+++ src/reducers/AccountReducer.js
@@ -1,5 +1,5 @@
-import {SIGN_IN, SIGN_UP, SIGN_OUT} from '../actions/AccountActions';
-import {setAccount, setToken, setRefreshToken, removeAccount, removeToken, removeRefreshToken} from '../helpers/account';
+import {SIGN_IN, SIGN_UP, SIGN_OUT, INIT_ACCOUNT} from '../actions/AccountActions';
+import {setAccount, setToken, setRefreshToken, removeAccount, removeToken, removeRefreshToken, getAccount} from '../helpers/account';
 
 const initialState = {
     account: null,
@@ -22,12 +22,16 @@ export default function(state = initialState, action){
             if(token) setToken(token);
             if(refreshToken) setRefreshToken(refreshToken);
 
-            return { ...initialState, account};
+            return { ...state, account};
         case SIGN_OUT: {
             removeAccount();
             removeToken();
             removeRefreshToken();
-            return { ...initialState, account: null};
+            return { ...state, account: null};
+        }
+        case INIT_ACCOUNT :{
+            const account = getAccount();
+            return { ...state, account};
         }
         default:
             return state;
