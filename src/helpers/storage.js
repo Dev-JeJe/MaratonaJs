d10a2b0dfe2fe532d704c1cc822feac0c3bddc94
Jefferson Dias Fraga <jeffycordan@gmail.com>
2020-07-01 22:49:51 -0300

    Signup stores account and token in cookies.

---
+++ src/helpers/storage.js
@@ -0,0 +1,25 @@
+/*operações referentes a cookie*/
+import Cookies from 'universal-cookie';
+
+const cookie = new Cookies();
+
+const defaultOptions = {
+    path: '/',
+};
+
+export const getCookie = (name, options) => {
+    if(!name) return null;
+
+    return cookie.get(name, {...defaultOptions, ...options}); //o que é spred
+};
+
+export const setCookie = (name, value, options) => {
+    if (!name || value == undefined) return null;
+    cookie.set(name, value, {...defaultOptions, ...options});
+    return true;
+};
+
+export const removeCookie = (name, options) => {
+    if(!name) return null;
+    cookie.remove(name, {...defaultOptions, ...options});
+};
